6611645250cab2d339c5c1396b3d976de45308e3
WillisGC <gc.yip@yahoo.com>
2019-12-19 17:19:34 +0800

    created memorygameactivity and insert photos in drawable

---
+++ app/src/main/java/com/example/team8memorygame/MemoryGameActivity.java
@@ -0,0 +1,14 @@
+package com.example.team8memorygame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MemoryGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memory_game);
+    }
+}
