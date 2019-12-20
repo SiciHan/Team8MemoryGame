1d3a85a6ae1b8b212a7a39f9623d5d721fa32ad2
JustinYu2019 <e0003064@u.nus.edu>
2019-12-20 14:04:50 +0800

    added button to page 2

--- app/src/main/java/com/example/team8memorygame/MainActivity.java
+++ app/src/main/java/com/example/team8memorygame/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.team8memorygame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1=findViewById(R.id.MoveToGameBtn);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,MemoryGameActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
